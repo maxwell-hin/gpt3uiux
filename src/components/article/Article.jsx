2f650386778706bb7791d8b9b8960adca7b3b493
Maxwell_Law <lchmaxwell@gmail.com>
2023-04-03 00:08:11 +0800

    Finished

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -11,9 +11,9 @@ const Article = ({ imgUrl, date, title }) => {
       <div className="gpt3__blog-container_article-content">
         <p>{date}</p>
         <h3>{title}</h3>
-      </div>
 
-      <p>Read Full Article</p>
+        <p>Read Full Article</p>
+      </div>
     </div>
   );
 };
